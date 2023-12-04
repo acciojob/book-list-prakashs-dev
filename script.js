3d8e4a245aad4064168db9683bba8516c5d3b5ce
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-04 15:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your JS code here. If required.
+const form = document.querySelector(".form-group");
+      const submit = document.getElementById("submit");
+      const tbody = document.getElementById("book-list");
+
+      function deleteRecod(event) {
+        const deleteButton = event.target;
+        const record = deleteButton.parentNode;
+        console.log(record);
+        record.remove();
+      }
+
+      const addBook = (event) => {
+        event.preventDefault();
+        const obj = {
+          title: form["title"].value,
+          author: form["author"].value,
+          isbn: form["isbn"].value,
+        };
+        form.reset();
+        createNewRecord(obj);
+      };
+
+      function createNewRecord(data) {
+        const row = document.createElement("tr");
+        for (let key in data) {
+          const cell = document.createElement("td");
+          cell.innerText = data[key];
+          row.appendChild(cell);
+        }
+        const clear = document.createElement("td");
+        clear.innerText = "X";
+        clear.className = "delete";
+        clear.addEventListener("click", deleteRecod);
+        row.appendChild(clear);
+        tbody.appendChild(row);
+      }
+      submit.addEventListener("click", addBook);
